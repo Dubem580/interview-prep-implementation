bbc0ce98d8251d724ae8390d2eea3b4a4ab36ea1
Dubem <dubemokam@gmail.com>
2017-10-18 21:31:32 -0700

    urlify

--- chapter1-arrays-strings/1.java
+++ chapter1-arrays-strings/1.java
@@ -26,7 +26,7 @@ public static void main(String[] args) {
     }
 }
 
-//check the permutation: given two strings, write a method to decide if one is a permutation of the other.
+//check  permutation: given two strings, write a method to decide if one is a permutation of the other.
 public static void main(String[] args) {
         String s = scanner.next();
         scanner.nextLine();
@@ -54,5 +54,48 @@ public static void main(String[] args) {
 }
 
 
+//write a method to replace all sapces in a string with "%20". you may assume that the string as sufficient space at the end to hold the additional characters, and that you are given the 'true'length of the string.
 
+public static void main(String[] args) {
+
+        String s = "Times are changing to suit the youth and their dreams                                  ";
+        char[] s1 = s.toCharArray();
+        int tru = 53;
+
+        System.out.println("before replacing spaces = " + s);
+
+        //replaceSpace(s1,19);
+        System.out.println("after replacing with %20 =\n" + replaceSpace(s1,tru));
+
+
+
+
+    }
+
+
+    public static String replaceSpace(char[] s, int tru) {
+        int spaces = 0, index = 0;
+        for (int i = 0; i < tru; i++) {
+            if (s[i] == ' ') {
+                spaces++;
+            }
+        }
+        index = tru + spaces * 2;
+        if (tru < s.length){
+            s[tru] = '\0';
+        }
+        for (int i = tru-1; i >= 0; i--) {
+            if (s[i] == ' ') {
+                s[index - 1] = '0';
+                s[index - 2] = '2';
+                s[index - 3] = '%';
+                index = index - 3;
+            } else {
+                s[index - 1] = s[i];
+                index--;
+            }
+        }
+        return new String(s);
+    }
+}
 
