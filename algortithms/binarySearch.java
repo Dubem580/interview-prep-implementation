4bcd7febffd5f703290f9ecbba640a1c4f49f845
Dubem <dubemokam@gmail.com>
2017-10-22 01:56:26 -0700

    added test for binarySearch

--- algortithms/binarySearch.java
+++ algortithms/binarySearch.java
@@ -1,4 +1,27 @@
-public static int rank(int key, int []a){
+//binary search algorithm. i added a test using scanner. remember binary search works when elements are sorted. give it a try.
+package com.lesson;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int [] a = new int[10];
+
+        System.out.println("enter 10 numbers in order:");
+        for (int i =0; i< a.length;i++){
+            a[i] = scanner.nextInt();
+            scanner.nextLine();
+        }
+
+        System.out.println("enter number to find");
+        int key = scanner.nextInt();
+        System.out.println(rank(key,a));
+    }
+
+    public static int rank(int key, int []a){
         int lo = 0;
         int hi = a.length-1;
         while(lo <= hi){
@@ -13,3 +36,4 @@ public static int rank(int key, int []a){
         }
         return -1;
     }
+}
